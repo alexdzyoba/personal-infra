c9da0d1ad1cbcea414e2ac499389ca235af436a0
Alex Dzyoba <alex@dzyoba.com>
2020-07-30 19:14:13 +0300

    site: Add DNS configuration for dzyoba.com

--- site/main.tf
+++ site/main.tf
@@ -21,3 +21,35 @@ provider "aws" {
     role_arn = "arn:aws:iam::${var.master_account_id}:role/OrganizationAccountAccessRole"
   }
 }
+
+resource "aws_route53_zone" "site" {
+  name = "dzyoba.com"
+}
+
+resource "aws_route53_record" "alex" {
+  zone_id = aws_route53_zone.site.zone_id
+  name    = "alex"
+  type    = "CNAME"
+  ttl     = "10800"
+  records = ["dzeban.gitlab.io"]
+}
+
+resource "aws_route53_record" "gitlab_pages_verification" {
+  zone_id = aws_route53_zone.site.zone_id
+  name    = "_gitlab-pages-verification-code.alex"
+  type    = "TXT"
+  ttl     = "10800"
+  records = [
+    var.gitlab_pages_verification_record
+  ]
+}
+
+resource "aws_route53_record" "google_analytics_verification" {
+  zone_id = aws_route53_zone.site.zone_id
+  name    = ""
+  type    = "TXT"
+  ttl     = "10800"
+  records = [
+    var.google_analytics_verification_record
+  ]
+}
--- site/vars.tf
+++ site/vars.tf
@@ -2,3 +2,13 @@ variable master_account_id {
   description = "Master account id that is trusted by OrganizationAccountAccessRole"
   type        = string
 }
+
+variable google_analytics_verification_record {
+  description = "Unique string value for TXT DNS record to verify domain for Google Analytics"
+  type        = string
+}
+
+variable gitlab_pages_verification_record {
+  description = "Unique string value for TXT DNS record to verify domain for Gitlab Pages"
+  type        = string
+}
