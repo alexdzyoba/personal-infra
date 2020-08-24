7d5716feb604faaf6443eab12847746f33b3db4f
Alex Dzyoba <alex@dzyoba.com>
2020-08-24 19:02:10 +0300

    site: Add Lambda for SES forwarding
    To deliver incoming messages via SES we use Lambda function that will
    forward email messages from SES to my gmail account.
    
    Lambda function is based on https://github.com/arithmetric/aws-lambda-ses-forwarder
    
    This lambda function was deployed manually with aws cli to the bucket.
    
    Besides the lambda function itself there are quite a few IAM resources
    here:
    
    1. IAM role for lambda function with assume role policy.
    2. IAM role policy that contains permissions allowing S3, SES and
       CloudWatch access. This policy is known in AWS documentation as
       "execution role", though it's not a role but a policy :shrug:.
    3. Permission for resource-based policy that allow SES to invoke this
       lambda.

--- site/email.tf
+++ site/email.tf
@@ -51,3 +51,82 @@ resource "aws_ses_domain_dkim" "site" {
   domain = aws_ses_domain_identity.site.domain
 }
 
+resource aws_lambda_function "forwarder" {
+  function_name = "ses-forwarder"
+
+  s3_bucket = "dzyoba-com-emails"
+  s3_key    = "forwarder.zip"
+
+  runtime = "nodejs12.x"
+  handler = "index.handler"
+
+  role = aws_iam_role.lambda_forwarder.arn
+
+  timeout = 30
+}
+
+resource aws_iam_role "lambda_forwarder" {
+  name = "LambdaSESForwarder"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "allow_logs_ses_s3" {
+  name = "AllowLogsSESS3"
+  role = aws_iam_role.lambda_forwarder.id
+
+  policy = <<-EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "AllowLogs",
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogStream",
+        "logs:CreateLogGroup",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Sid": "AllowS3Access",
+      "Effect": "Allow",
+      "Action": [
+        "s3:GetObject",
+        "s3:PutObject"
+      ],
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.emails.id}/*"
+    },
+    {
+      "Sid": "AllowSESSend",
+      "Effect": "Allow",
+      "Action": "ses:SendRawEmail",
+      "Resource": "arn:aws:ses:${data.aws_region.current.name}:${var.master_account_id}:identity/*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_permission" "allow_ses" {
+  statement_id   = "AllowExecutionFromSES"
+  action         = "lambda:InvokeFunction"
+  function_name  = aws_lambda_function.forwarder.function_name
+  principal      = "ses.amazonaws.com"
+  source_account = var.master_account_id
+}
